20ea5ff311741a07371def169b42675727951ebb
deceive0112 <94655133+deceive0112@users.noreply.github.com>
2026-03-03 19:55:40 +0800

    TindaTrack set-up

---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class _layout extends Component {
+  render() {
+    return (
+      <div>_layout</div>
+    )
+  }
+}
+
+export default _layout
---
+++ app/tabs/sales.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function Sales() {
+  return (
+    <View>sales</View>
+  )
+}
+
+export default Sales
