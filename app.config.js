7b58455d58f89b4f5b4d23fc6ac5a44019b8c7fd
deceive0112 <94655133+deceive0112@users.noreply.github.com>
2026-03-06 20:42:15 +0800

    fix: hardcode fallback

--- app.config.js
+++ app.config.js
@@ -32,8 +32,8 @@ export default {
       versionCode: 5,
     },
     extra: {
-      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
-      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL || "https://bzqwwrwuhiquntoevyha.supabase.co",
+      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6cXd3cnd1aGlxdW50b2V2eWhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2MzEyOTMsImV4cCI6MjA4ODIwNzI5M30.p7ntwFnI0dTXkKPVyVYGFmKdSPL6usSN-2jdpJx8sRQ",
       router: {},
       eas: {
         projectId: "2284143b-c8d1-4591-b67a-e5793b9bec6d",
