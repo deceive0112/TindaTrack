b28e09baa9587da4cdecbbc4f748ce709f7ebcc3
deceive0112 <94655133+deceive0112@users.noreply.github.com>
2026-03-06 12:10:13 +0800

    fix: EAS

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,14 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+// Allow .ts and .tsx files from node_modules
+config.resolver.sourceExts = [
+  ...config.resolver.sourceExts,
+  "ts",
+  "tsx",
+  "mts",
+];
+
+module.exports = withNativeWind(config, { input: "./global.css" });
