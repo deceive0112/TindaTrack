387beb6cc0de98dddbaf62f83fb47fc6331f0e5f
deceive0112 <94655133+deceive0112@users.noreply.github.com>
2026-03-06 16:48:03 +0800

    updates

---
+++ app.config.js
@@ -0,0 +1,43 @@
+export default {
+  expo: {
+    name: "TindaTrack",
+    slug: "tindatrack",
+    scheme: "tindatrack",
+    version: "1.0.0",
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    experiments: {
+      tsconfigPaths: true,
+    },
+    plugins: ["expo-router", "expo-sharing", "expo-font"],
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      package: "com.deceive0112.tindatrack",
+      adaptiveIcon: {
+        foregroundImage: "./assets/icon.png",
+        backgroundColor: "#ffffff",
+      },
+      versionCode: 5,
+    },
+    extra: {
+      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+      router: {},
+      eas: {
+        projectId: "2284143b-c8d1-4591-b67a-e5793b9bec6d",
+      },
+    },
+  },
+};
